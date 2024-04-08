a5a73481d1e3f7905e8d88c972bb53d9e9977015
Maro2058 <57037832+Maro2058@users.noreply.github.com>
2024-04-08 14:33:23 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
+
+class Book{
+private:
+    int ISBN;
+    string title;
+    string author;
+    string publisher;
+    enum genre {Art = 1, Biology, BusinessAndEconomics, Chemistry, ComputerScience, Education, Engineering,
+        HealthAndNutrition, Language, Mathematics, Music, Physics, Psychology};
+    int publicationYear;
+    bool availabilityStatus;
+
+public:
+    void setISBN(int num){
+        ISBN = num;
+    }
+    int getISBN(){
+        return ISBN;
+    }
+};
 
 class Member{
 private:
     string userID;
     string userName;
-    int pass;
+    int password;
+    Book *checkedOut;
+    int fines;
 
 public:
     void login(){
@@ -21,15 +46,7 @@ public:
     }
 };
 
-class Book{
-private:
-    int ISBN;
-    string title;
-    string author;
-    enum genre {HealthAndNutrition, Physics, Biology, Chemistry, Engineering, ComputerScience, Art, Music, Mathematics, BusinessAndEconomics, Psychology, Education,  Language};
 
-public:
-};
 
 class Student : public Member{
 private:
@@ -47,8 +64,32 @@ public:
 class Librarian : public Member{
 private:
 
+
 public:
-    Book addBook(){
+    Book addBook(){ // Adds book to file and returns Book
+        int num;
+        int anyInput;
+        Book tempBook;
+        fstream file("output.txt", ios::in);
+
+        if (file.is_open()){
+            cout << "Enter the Books Info:\n ISBN:";
+            cin >> num;
+            string temp  = reinterpret_cast<const char*>(num);
+
+            while(temp.length() != 13){
+                cout << "Error! Make sure the ISBN is 13 digits" << endl;
+                cout << "Enter the Books Info:\n ISBN:";
+                cin >> num;
+                temp  = reinterpret_cast<const char*>(num);
+            }
+            tempBook.setISBN(num);
+
+
+        } else{
+            cout << "File Failed to Open\n Press any Number to continue"<<endl;
+            cin >> anyInput;
+        }
 
     }
     void removeBook(Book& book){
@@ -61,7 +102,7 @@ public:
 
     }
     void addMember(){
-        Member* mmbrRef = new Member;
+        Member* memberRef = new Member;
     }
     void removeMember(Member member){
 
@@ -78,12 +119,25 @@ public:
 
 class Loan{
 private:
+    int memberID;
+    int bookISBN;
+    time_t borrowDate;
+    time_t dueDate;
 
 public:
+    void setBorrowDate(){
+        borrowDate = time(nullptr);
+        if(borrowDate == -1){ //Error Handling, in case time() returns error value
+
+        }
+    }
+
 };
 
 int main()
 {
+    Librarian librarian;
+    librarian.addBook();
     cout<<"Hello World";
 
     return 0;
